b98be7cee10c8bc329e0074f06ebad965f0bb7d5
Nexus0622 <natekofoed@gmail.com>
2022-10-22 08:52:33 -0400

    not much here lmao

---
+++ frontend/src/URL.js
@@ -0,0 +1,4 @@
+export default function URL()
+{
+    return "mongodb+srv://Nexus:june0622@canvasapp.fq8cccu.mongodb.net/?retryWrites=true&w=majority"
+}
---
+++ frontend/src/result.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import URL from './URL';
+
+let url = URL();
+
+export default function Result ()
+{
+    return(
+        <div style={{marginTop:"10%", borderBottom:"solid 3px blue", width:"70%", height:"100%"}}>
+           Sounds
+        </div>
+    )
+}
