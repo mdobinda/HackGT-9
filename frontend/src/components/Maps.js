48c48649d5636bab0e3cdcc048f4652e14ceb223
Dylan <dylan.ochoa2323@gmail.com>
2022-10-22 03:22:16 -0400

    Fixed map to throw a pin down on GTech

--- frontend/src/components/Maps.js
+++ frontend/src/components/Maps.js
@@ -1,9 +1,39 @@
 import {Apiconfig} from '../Apiconfig'
+import GoogleMap from 'google-map-react';
+import {FmdGood} from '@mui/icons-material';
+import React from "react";
+
+const Marker = ({ text }) => {return (<div className="pin">
+<FmdGood className="pin-icon" />
+</div>)}
 
 
 const Maps = () => {
-return <iframe title="yay" width="600" height="450" style={{border: 0}} loading="lazy" allowfullscreen
-        src={`https://www.google.com/maps/embed/v1/place?key=${Apiconfig.mapsKey}&q=Eiffel+Tower,Paris+France`}></iframe>
+
+    const defaultProps = {
+        center: {
+                lat: 33.7756,
+                lng: -84.3963
+        },
+        zoom: 11
+    };
+
+
+    return (
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMap
+            bootstrapURLKeys={{ key: Apiconfig.mapsKey }}
+            defaultCenter={defaultProps.center}
+            defaultZoom={defaultProps.zoom}
+            >
+                <Marker
+                lat={33.7756}
+                lng={-84.3963}
+                text={"My Text"}
+                />
+            </GoogleMap>
+        </div>
+    )
 }
 
 export default Maps
