5a3d929adb973c85760d04d215627cd0562e2824
Nexus0622 <natekofoed@gmail.com>
2022-10-22 16:46:12 -0400

    Minnie image

--- frontend/src/survey/Feeling.js
+++ frontend/src/survey/Feeling.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './Survey.css';
+import feels from '../assets/feels.png'
 const layers = require("./questions.json");
 export default function Feeling()
 {
@@ -15,7 +16,8 @@ export default function Feeling()
                 <h1> What are you feeling right now?</h1>
                 <h4> If you don't know, just try your best</h4>
             </div>
-            <div style={{display:"flex", flexDirection:"column" ,alignContent:"center", justifyContent:"center", alignItems:"center"}}>
+            <img src={feels} style={{position:"absolute", zIndex:"-5", height:"55%", width:"35%", marginLeft:"3.5vw"}} />
+            <div style={{display:"flex", flexDirection:"column" ,alignContent:"center", justifyContent:"center", alignItems:"center", marginLeft:"20vw"}}>
                 {layers["root"].map((question) => {
                     return(
                         <div id={question} className={hideAns ? "Answer hidden" : "Answer"} onLoad={setTimeout(() => {setHideAns(false)}, 700)}>
