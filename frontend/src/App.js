acb0e5380657162971859d6b2db98046b034e67a
Dylan <dylan.ochoa2323@gmail.com>
2022-10-22 14:45:01 -0400

    Initialized a card with the safe spot location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
     
     <Routes>
       <Route path='/' element={<Home/>}/> 
-      <Route path='/map' element={<Maps/>}/>
+      <Route path='/training' element={<Maps/>}/>
       </Routes>
     
     
