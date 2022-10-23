4e886c5d435dccce268d6c870db3a7836dc75058
Dylan <dylan.ochoa2323@gmail.com>
2022-10-23 03:19:51 -0400

    Added the ability to retrieve mongodb chunks for the audio file

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,16 @@
-from json import JSONEncoder
 from fastapi import FastAPI, APIRouter, Request
 from dotenv import load_dotenv
-from pymongo import MongoClient
 from functions import connectToDB
-from models import Sounds
-from typing import List
+from bson.objectid import ObjectId
 
 router = APIRouter()
 load_dotenv()
 app = FastAPI()
+fs = connectToDB(app)
 
 
 @app.on_event("startup")
 def startup_db_client():    
-    fs = connectToDB(app)
     for doc in app.database['fs.files'].find():
         print(doc)
     print("Connected to the MongoDB database!") 
@@ -22,13 +19,21 @@ def startup_db_client():
 def shutdown_db_client():
     app.mongodb_client.close()
 
-@app.get("/", response_description="List all sounds")
+@app.get("/list", response_description="List all sounds")
 def list_books(request: Request):
     sounds = {}
     for doc in app.database['fs.files'].find():
         print(doc["filename"])
         sounds[sanitize(doc["filename"])] = str(doc["_id"])     
     return sounds
+
+@app.get("/retrieve", response_description="Get a single sound by id")
+def find_sound(id: str, request: Request):
+    print(id)
+    out = fs.get(ObjectId(id)).read()
+    print(out)
+
+    return {'mp3', str(out)}
 
 def sanitize(path):
     arr = path.split('/')
